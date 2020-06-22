3be1e0853f0134c13f06f9b74a47a91440d081c1
Rafael <rafa@blackmagicbox.com.br>
2020-06-22 02:17:58 +0200

    Initial Commit

---
+++ i-am-pickle-rick/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  i-am-pickle-rick
+//
+//  Created by Rafael Santos on 22.06.20.
+//  Copyright © 2020 Rafael Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
